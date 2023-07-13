394e8ca1db736c137d8b4090a16d091a2cb71341
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-07-13 15:56:21 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@clerk/nextjs"
 import Link from "next/link"
 
-export default function Home() {
+export default async function Home() {
+
+  const {userId} = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white ">
       <div className="w-full max-w-[1000px] max-auto">
@@ -12,7 +18,7 @@ export default function Home() {
           Best app for tracking your thoughts and reflect on them.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               Get started
             </button>
